47057dd73ede23e795688d3d8467b1eaa18edd69
Anton Shnip <toni.mr.anton@mail.ru>
2022-09-30 13:07:46 +0300

    Change start file and delete unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import DB from './db/db';
-import Helpers from './util/helpers';
 import { Blockchain } from './blockchain';
 import { Utxo } from './utxo';
 
 const blockchain = new Blockchain();
 const utxo = new Utxo();
-
-// console.log(utxo.getUtxoByAddress('020983a291f7997c1d326cc6fb7b3567bb183f79f0d3b4eeb026df9f7b6cb3e49b'));
 
 async function start(index) {
   const block = await blockchain.getBlockByIndex(index, true)
