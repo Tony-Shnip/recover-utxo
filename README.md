852f4a605c3360191ac617824251296aaa0bf7f3
Anton Shnip <toni.mr.anton@mail.ru>
2022-09-30 13:01:14 +0300

    Add readme file and change start function params

---
+++ README.md
@@ -0,0 +1,20 @@
+# Recover-utxo
+
+### Requirements
+1. Node.js 10.24.1
+
+### Installation
+
+```
+npm install
+```
+
+### Start
+
+#### Setup environment
+Create ```.env``` file by ```.env.example``` in the root folder.
+
+#### Development\Production
+```
+npm run start BLOCK_INDEX
+```
--- src/index.ts
+++ src/index.ts
@@ -8,31 +8,16 @@ const utxo = new Utxo();
 
 // console.log(utxo.getUtxoByAddress('020983a291f7997c1d326cc6fb7b3567bb183f79f0d3b4eeb026df9f7b6cb3e49b'));
 
+async function start(index) {
+  const block = await blockchain.getBlockByIndex(index, true)
 
+  for (let i = 0; i < block.data.length; i++) {
+    if (block.data[i].type === 'regular' && block.data[i].txOuts.length > 2) {
+      utxo.checkUtxo(block.data[i]);
+    }
+  }
+}
 
-
-
-
-// blockchain.getBlockByIndex(2, true)
-//   .then((res) => {
-//     console.log(res.data[1].txOuts);  
-//   })
-
-
-
-
-
-
-// async function start(index) {
-//   const block = await blockchain.getBlockByIndex(index, true)
-
-//   for (let i = 0; i < block.data.length; i++) {
-//     if (block.data[i].type === 'regular' && block.data[i].txOuts.length > 2) {
-//       utxo.checkUtxo(block.data[i]);
-//     }
-//   }
-// }
-
-// start(2)
+start(process.argv[2])
 
 
